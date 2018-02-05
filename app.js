f562b125a620ad726ec1a34490777e77469fcb08
m.flueh <mf@esyoil.com>
2018-02-05 22:19:25 +0100

    add static latitude and longitude

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode.js');
@@ -20,5 +21,26 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     console.log(errorMessage);
   } else {
     console.log(JSON.stringify(results, undefined, 2));
+  }
+});
+
+// 673f1e94bcd88ff299541cd365fa2881
+// https://api.darksky.net/forecast/673f1e94bcd88ff299541cd365fa2881/53.2536096,%2010.3885867
+*/
+
+// Start here
+
+const request = require('request');
+
+request({
+  url: `https://api.darksky.net/forecast/673f1e94bcd88ff299541cd365fa2881/53.2536096,%2010.3885867`,
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect server.');
+  } else if (response.statusCode === 400) {
+    console.log('Unable to fetch weather.');
+  } else if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   }
 });
